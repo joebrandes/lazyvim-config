2536e3506b91cec46b4619914055908e566185e5
Joe Brandes <joe.brandes@gmail.com>
2023-04-18 16:39:27 +0200

    ADD colorscheme.lua and updates

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+  -- activate catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+  -- add onedark
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      -- Main Options
+      style = "deep",
+      -- toggle theme style ---
+      toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+      toggle_style_list = {
+        "dark",
+        "darker",
+        "cool",
+        "deep",
+        "warm",
+        "warmer",
+        "light",
+      }, -- List of styles to toggle between
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
